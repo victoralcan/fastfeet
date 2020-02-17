35f46ab9d17631128facc6a5de7dba8d224dac4a
victoralcan <victoralcan@gmail.com>
2020-02-17 14:58:38 -0300

    Adicionando algumas validaçoes de login e cadastro

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,22 @@
 import Recipient from '../models/Recipient';
+import User from '../models/User';
 
 class RecipientController {
   async store(req, res) {
+    if (!req.userId) {
+      return res.status(401).json({ error: 'You are not logged in' });
+    }
+
+    const admin = User.findOne({
+      where: {
+        email: 'admin@fastfeet.com',
+      },
+    });
+
+    if (admin.id !== req.userId) {
+      return res.status(403).json({ error: 'You are not admin' });
+    }
+
     const recipient = await Recipient.create(req.body);
 
     return res.json(recipient);
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,6 +2,16 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const exist = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (exist) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
     const user = await User.create(req.body);
 
     const { id, name, email } = user;
