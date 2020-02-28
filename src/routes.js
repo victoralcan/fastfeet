5f328d3efc5eed8d87c7458cef44c4019982f0ce
Victor Alcantara <victoralcan@localhost.localdomain>
2020-02-28 20:49:26 -0300

    Avatar para entregadores. Variaveis de Ambiente

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 import AuthMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
+
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
@@ -18,5 +23,7 @@ routes.post('/deliverymans/cadastro', DeliverymanController.store);
 routes.get('/deliverymans', DeliverymanController.index);
 routes.put('/deliverymans/:id', DeliverymanController.update);
 routes.delete('/deliverymans/:id', DeliverymanController.delete);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
